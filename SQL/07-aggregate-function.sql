efa37a5eea75212d031d25b7b2a4b1205f4e8cd0
Revig <giverbro13@gmail.com>
2023-04-16 19:03:52 -1200

    aggregate functions in sql

---
+++ SQL/07-aggregate-function.sql
@@ -0,0 +1,22 @@
+-- Some aggregate functions in SSQL are: count(), avg(), sum(), min(), maax(), etc.
+-- Show the average salary of instructors from IT department
+SELECT avg(salary) from instructor where dept_name="IT"
+-- Show maximum and minimum ID from student
+SELECT max(ID), min(ID) from student
+-- Show total salary of all instructors
+SELECT sum(salary) from instructor
+-- Show maximum salary of all intructors
+SELECT max(salary) from instructor
+-- Show minimum salary of all instructors
+SELECT min(salary) from instructor
+-- Show average salary of all intructors
+SELECT avg(salary) from instructor
+-- Show total number of records in department table
+-- Here, "*" means an entire row/tuple
+SELECT count(*) from department 
+select * from department
+select * from instructor
+-- Show total number intructors from "Physics" department
+select count(*) from instructor where dept_name="Physics"
+-- Show total salary of intructors from "IT" department
+select sum(salary) from instructor where dept_name="IT"
