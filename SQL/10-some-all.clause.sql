d7919ff857847f554515a8ecc8a23c8961b44c3c
Revig <giverbro13@gmail.com>
2023-04-25 12:27:07 +0545

    correct

--- SQL/10-some-all.clause.sql
+++ SQL/10-some-all.clause.sql
@@ -1,4 +1,4 @@
 select salary from instructor where salary < all(select salary from instructor where address="Kathmandu");
 
 -- Gives department name with higest average salry
-select dept_nmae from instructor group by dept_name having avg(salary) >= all(select avg(salary)) from instructor group by dept_name);
+select dept_name from instructor group by dept_name having avg(salary) >= all(select avg(salary) from instructor group by dept_name);
