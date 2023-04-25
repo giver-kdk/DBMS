35a35cfaf922b506bf426366a9e09198cf30bce3
Revig <giverbro13@gmail.com>
2023-04-25 13:01:54 +0545

    modification sql

--- SQL/13-modification-advance.sql
+++ SQL/13-modification-advance.sql
@@ -1,3 +1,5 @@
 delete from instructor where dept_name in (select dept_name from department where building="Block B");
+-- Inserts ID, name and department of instructor into student. Also, tot_credit is set to 0 
+-- We utilize already existing data
+insert into student select ID, name, dept_name, 0 from instructor;
 
-insert into 
