ac4a2aec55519ad2cca19f2f8191e1b1884cbc11
Revig <giverbro13@gmail.com>
2023-04-26 13:50:13 +0545

    view update

--- SQL/14-view.sql
+++ SQL/14-view.sql
@@ -1,4 +1,5 @@
 -- Creates a faculty view with data ID, name and dept_name
+-- Here, salary of instructor is hidden(not shown)
 create view faculty as select ID, name, dept_name from instructor;
 -- Actually retrieve the viewable data
 select * from faculty;
@@ -7,3 +8,8 @@ create view dept_total_salary(dept_name, total_salary) as select dept_name, sum(
 
 select * from dept_total_salary;
 
+-- Shows all the created views in the database
+SHOW FULL TABLES IN csit77_giver WHERE TABLE_TYPE LIKE 'VIEW'
+
+-- NOTE: VIEW can also be modified like insertion, update, deletion, set operation, etc.
+-- But, we prefer to modify the original table instead of VIEW
