8895465f119f624d20711fa44958e5ff7cecd831
Revig <giverbro13@gmail.com>
2023-04-26 14:04:57 +0545

    natural

--- SQL/15-join.sql
+++ SQL/15-join.sql
@@ -1,1 +1,5 @@
+-- The column data with same value wil occur once only naturally
 select name, building from instructor natural join department;
+
+-- Joins all the rows of two tables when condition is satisfied
+select name, building from instructor inner join department on instructor.dept_name = department.dept_name
