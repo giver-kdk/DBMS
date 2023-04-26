ddc2a360b4c923ccec05a4946bc8809bd6747126
Revig <giverbro13@gmail.com>
2023-04-26 14:15:46 +0545

    right

--- SQL/15-join.sql
+++ SQL/15-join.sql
@@ -5,7 +5,11 @@ select name, building from instructor join department
 select name, building from instructor natural join department;
 
 -- Joins all the rows of two tables when condition is satisfied
+-- INNER Join is same as JOIN
 select name, building from instructor inner join department on instructor.dept_name = department.dept_name;
 
 -- 
 select * from instructor left outer join department on instructor.dept_name = department.dept_name
+
+-- 
+select * from instructor right outer join department on instructor.dept_name = department.dept_name
