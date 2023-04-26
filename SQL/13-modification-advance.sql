028a310f0fdbec32034ec1115ad50a80d3bd0c2c
Revig <giverbro13@gmail.com>
2023-04-26 13:34:45 +0545

    lol

--- SQL/13-modification-advance.sql
+++ SQL/13-modification-advance.sql
@@ -2,5 +2,5 @@ delete from instructor where dept_name in (select dept_name from department wher
 -- Inserts ID, name and department of instructor into student. Also, tot_credit is set to 0 
 -- We utilize already existing data
 insert into student select ID, name, dept_name, 0 from instructor;
-
+-- Increase salary of instructor by 5% if salary is less than average salary 
 update instructor set salary=1.05*salary where salary < (select avg(salary) from instructor);
