8894eddacbe5605387e1f62843dca683344457e7
Revig <giverbro13@gmail.com>
2023-04-26 14:14:22 +0545

    left

--- SQL/15-join.sql
+++ SQL/15-join.sql
@@ -1,5 +1,11 @@
+-- Does cartesian product of all data
+select name, building from instructor join department
+
 -- The column data with same value wil occur once only naturally
 select name, building from instructor natural join department;
 
 -- Joins all the rows of two tables when condition is satisfied
-select name, building from instructor inner join department on instructor.dept_name = department.dept_name
+select name, building from instructor inner join department on instructor.dept_name = department.dept_name;
+
+-- 
+select * from instructor left outer join department on instructor.dept_name = department.dept_name
