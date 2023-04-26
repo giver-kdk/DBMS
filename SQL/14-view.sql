a63a2a3d1a7bb975afdee3c7d903d1aae8e96e25
Revig <giverbro13@gmail.com>
2023-04-26 13:42:26 +0545

    view

---
+++ SQL/14-view.sql
@@ -0,0 +1,9 @@
+-- Creates a faculty view with data id, name and dept_name
+create view faculty as select id, name, dept_name from instructor;
+-- Actually retrieve the viewable data
+select * from faculty;
+-- Create view with custom calculated attribute from already existing attribute
+create view dept_total_salary(dept_name, total_salary) as select dept_name, sum(salary) from instructor group by dept_name; 
+
+select * from dept_total_salary;
+
